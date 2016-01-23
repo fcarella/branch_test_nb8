5fcd4842ed65db20c89384026b28e56423201608
fred <fred@192.168.56.1>
2016-01-23 11:12:24 -0500

    start

---
+++ src/main/java/com/mycompany/branch_test_nb8_github/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.branch_test_nb8_github;
+
+/**
+ *
+ * @author fred
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Branch Test");
+    }
+}
