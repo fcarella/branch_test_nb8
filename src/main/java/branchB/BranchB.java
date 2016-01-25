ceb56009f15303c6caa4ff7b7c1739fcf10452ba
Fred Carella <fred.carella@saultcollege.ca>
2016-01-25 12:34:59 -0500

    branchB

---
+++ src/main/java/branchB/BranchB.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package branchA;
+
+/**
+ *
+ * @author fred
+ */
+public class BranchA {
+    private static String branch="BranchA";
+
+    /**
+     * @return the branch
+     */
+    public static String getBranch() {
+        return branch;
+    }
+
+    public BranchA() {
+    }
+    
+}
